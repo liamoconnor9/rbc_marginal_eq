a6b27e8d187f9d4c38f9441016620862243c2f40
Liam O'Connor <liamoconnor2025@u.northwestern.edu>
2021-03-01 10:37:46 -0800

    nominal 2d simulations for each ra

---
+++ RA4E9/rbc_sim_nom_4e9.sh
@@ -0,0 +1,17 @@
+#PBS -S /bin/bash
+#PBS -N rbc_sim_nom_4e9
+#PBS -l select=4:ncpus=28:mpiprocs=28:model=bro
+#PBS -l walltime=24:00:00
+#PBS -j oe
+module load mpi-sgi/mpt
+module load comp-intel
+export PATH=$HOME/scripts:$PATH
+deactivate
+unset PYTHONPATH
+source ~/miniconda3/etc/profile.d/conda.sh
+export PYTHONNOUSERSITE=1
+conda activate dedalus
+# support lots of text output to stdio for analysis
+export MPI_UNBUFFERED_STDIO=true
+cd ~/scratch/dedalus/EVP_evolution/RA4E9/
+mpiexec_mpt -np 112 python3 rbc_sim_nom.py
